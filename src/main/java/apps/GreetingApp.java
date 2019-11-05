c3e1b765e09c159db27e8d14cf0690968a78e21a
metellusa <metellusa@users.noreply.github.com>
2019-11-05 16:58:40 -0500

    Attempting auto deploying on Heroku

--- src/main/java/apps/GreetingApp.java
+++ src/main/java/apps/GreetingApp.java
@@ -2,7 +2,7 @@ package apps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// Hello world
 @SpringBootApplication
 public class GreetingApp {
  
